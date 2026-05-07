554c5aa069692010ce17ac12f66d4419913a4154
Tomoyuki Sakurai <y@trombik.org>
2026-05-07 11:15:52 +0700

    fixup

--- src/rokujo/lexis/formatters/base.py
+++ src/rokujo/lexis/formatters/base.py
@@ -5,9 +5,13 @@ from collections import Counter
 
 class OutputFormatter(ABC):
     @abstractmethod
-    def format(self, data: list) -> Any:
+    def format(self, data: list, line_ending: str = "\r\n") -> Any:
         """
         Perform formatting.
+
+        Args:
+            data: Data to format
+            line_ending: Line ending style ('\r\n' for CRLF, '\n' for LF)
         """
         pass
 
